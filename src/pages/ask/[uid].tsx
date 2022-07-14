d8f3c3eaa31c609de7da66ae1da4168f30a8f588
Theo Browne <me@t3.gg>
2022-07-13 19:38:51 -0700

    slightly less horrid question form

--- src/pages/ask/[uid].tsx
+++ src/pages/ask/[uid].tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { trpc } from "../../utils/trpc";
@@ -15,26 +16,40 @@ const AskForm: React.FC<{ userId: string }> = ({ userId }) => {
   if (isLoading) return null;
 
   return (
-    <div className="flex flex-col p-8 items-center">
-      <h1 className="text-2xl font-bold">Ask {data?.name} a question!</h1>
+    <>
+      <Head>
+        <title>Ask {data?.name} a question!</title>
+      </Head>
+      <div className="flex flex-col items-center text-center">
+        <div className="flex flex-col p-8 items-center bg-gray-900 border-2 w-full md:w-96 m-8">
+          <h1 className="text-2xl font-bold">Ask {data?.name} a question!</h1>
+          <h2>And someone pls make this page less ugly</h2>
 
-      <textarea
-        placeholder="Question"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          if (!question) return;
+          <div className="p-4" />
 
-          mutate({ userId, question });
+          <input
+            placeholder="Question"
+            className="p-1 px-2 w-full text-gray-800"
+            type="text"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+          <div className="p-2" />
+          <button
+            className="bg-gray-200 text-gray-800 p-4 rounded hover:bg-gray-100 font-bold flex gap-2"
+            onClick={() => {
+              if (!question) return;
 
-          setQuestion("");
-        }}
-      >
-        Submit!
-      </button>
-    </div>
+              mutate({ userId, question });
+
+              setQuestion("");
+            }}
+          >
+            Submit!
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
