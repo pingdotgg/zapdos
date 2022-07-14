f93fa486dcc26bdb22c587e3e5076c2473225bd8
Theo Browne <me@t3.gg>
2022-07-13 23:47:58 -0700

    cleanup

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,9 +1,7 @@
-// src/pages/api/trpc/[trpc].ts
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { appRouter } from "../../../server/router";
 import { createContext } from "../../../server/router/trpc/context";
 
-// export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext: createContext,
--- src/server/router/trpc/context.ts
+++ src/server/router/trpc/context.ts
@@ -1,36 +1,14 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { Session } from "next-auth";
 import { getZapdosAuthSession } from "../../common/get-server-session";
 import { prisma } from "../../db/client";
-interface CreateContextOptions {
-  session: Session | null;
-  prisma: typeof prisma;
-}
 
-/**
- * Inner function for `createContext` where we create the context.
- * This is useful for testing when we don't want to mock Next.js' request/response
- */
-export async function createContextInner(opts: CreateContextOptions) {
-  return { session: opts.session, prisma };
-}
-
-export type Context = trpc.inferAsyncReturnType<typeof createContextInner>;
-
-/**
- * Creates context for an incoming request
- * @link https://trpc.io/docs/context
- */
-export async function createContext(
-  opts: trpcNext.CreateNextContextOptions
-): Promise<Context> {
-  // for API-response caching see https://trpc.io/docs/caching
-
+export async function createContext(opts: trpcNext.CreateNextContextOptions) {
   const req = opts?.req;
   const res = opts?.res;
 
   const session = req && res && (await getZapdosAuthSession({ req, res }));
 
-  return await createContextInner({ session, prisma });
+  return { session, prisma };
 }
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
