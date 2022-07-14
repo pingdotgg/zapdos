69cfb1a9c5f8b5759b795b522927d87d0ace2df4
Theo Browne <me@t3.gg>
2022-07-14 00:13:49 -0700

    remove ssr as I don't really need it lol

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -75,32 +75,7 @@ export const trpc = setupTRPC<AppRouter, SSRContext>({
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: true,
-  /**
-   * Set headers or status code when doing SSR
-   */
-  responseMeta(opts) {
-    const ctx = opts.ctx as SSRContext;
-
-    if (ctx.status) {
-      // If HTTP status set, propagate that
-      return {
-        status: ctx.status,
-      };
-    }
-
-    const error = opts.clientErrors[0];
-    if (error) {
-      // Propagate http first error from API calls
-      return {
-        status: error.data?.httpStatus ?? 500,
-      };
-    }
-
-    // for app caching with SSR see https://trpc.io/docs/caching
-
-    return {};
-  },
+  ssr: false,
 });
 
 /**
