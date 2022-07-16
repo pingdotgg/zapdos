393d7615a98bde92f556b5b7159206f3f20eeb18
MirkoPassoli <mirko.passoli@gmail.com>
2022-07-16 17:02:38 +0200

    Impl. trcp api to get/set required login setting

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,9 +1,11 @@
 // src/server/router/index.ts
 import { newQuestionRouter } from "./subroutes/question";
+import { SettingsRouter } from "./subroutes/settings";
 import { t } from "./trpc";
 
 export const appRouter = t.router({
   questions: newQuestionRouter,
+  settings: SettingsRouter,
 });
 
 // export type definition of API
