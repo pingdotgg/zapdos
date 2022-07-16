2d306d30eb965b6b419a8262e3db5d1f313327a3
Pierre Bérubé <pierre@lgse.com>
2022-07-16 15:30:37 -0600

    add usePusherStore hook and logic to use existing pusher client when it exists

--- src/utils/pusher.tsx
+++ src/utils/pusher.tsx
@@ -4,54 +4,59 @@
  * This defines a Pusher client and channel connection as a vanilla Zustand store.
  */
 import Pusher, { Channel, PresenceChannel } from "pusher-js";
-import reactZustandCreate from "zustand";
 import vanillaCreate, { StoreApi } from "zustand/vanilla";
 
 const pusher_key = process.env.NEXT_PUBLIC_PUSHER_APP_KEY!;
+const pusher_server_host = process.env.NEXT_PUBLIC_PUSHER_SERVER_HOST!;
+const pusher_server_port = parseInt(process.env.NEXT_PUBLIC_PUSHER_SERVER_PORT!, 10);
 
 interface PusherZustandStore {
   pusherClient: Pusher;
   channel: Channel;
   presenceChannel: PresenceChannel;
-
   members: { [key: string]: any };
 }
 
-// self-contained Pusher state machine that manages:
-// - Pusher Client
-// - Connected/subscribed channels
-// - "Current members" via presence
-const createPusherStore = (slug: string, publishPresence?: boolean) => {
-  const randomUserId = `random-user-id:${Math.random().toFixed(7)}`;
-  const pusherClient = new Pusher(pusher_key, {
-    authEndpoint: "/api/pusher/auth-channel",
-    auth: {
-      headers: { user_id: randomUserId },
-    },
-    cluster: "us3",
-  });
+const usePusherStore = (slug: string) => {
+  let pusherClient : Pusher;
+  if (Pusher.instances.length) {
+    pusherClient = Pusher.instances[0] as Pusher;
+    pusherClient.connect();
+  } else {
+    const randomUserId = `random-user-id:${Math.random().toFixed(7)}`;
+    pusherClient = new Pusher(pusher_key, {
+      wsHost: pusher_server_host,
+      wsPort: pusher_server_port,
+      enabledTransports: ["ws"],
+      forceTLS: false,
+      disableStats: true,
+      authEndpoint: "/api/pusher/auth-channel",
+      auth: {
+        headers: { user_id: randomUserId },
+      },
+    })
+  }
+
   const channel = pusherClient.subscribe(slug);
 
   const presenceChannel = pusherClient.subscribe(
     `presence-${slug}`
   ) as PresenceChannel;
 
-  const newStore = vanillaCreate<PusherZustandStore>((set) => {
+  const store = vanillaCreate<PusherZustandStore>(() => {
     return {
       pusherClient: pusherClient,
       channel: channel,
       presenceChannel,
-
       members: {},
     };
   });
 
   // Update helper that sets 'members' to contents of presence channel's current members
   const updateMembers = () => {
-    newStore.setState(() => ({
+    store.setState(() => ({
       members: presenceChannel.members.members,
     }));
-    console.log("Current members updated:", presenceChannel.members.members);
   };
 
   // Bind all "present users changed" events to trigger updateMembers
@@ -59,7 +64,13 @@ const createPusherStore = (slug: string, publishPresence?: boolean) => {
   presenceChannel.bind("pusher:member_added", updateMembers);
   presenceChannel.bind("pusher:member_removed", updateMembers);
 
-  return newStore;
+  useEffect(() => {
+    return () => {
+      pusherClient.disconnect();
+    };
+  }, []);
+
+  return store;
 };
 
 /**
@@ -68,11 +79,12 @@ const createPusherStore = (slug: string, publishPresence?: boolean) => {
  * This creates a "Zustand React Context" that we can provide in the component tree.
  */
 import createContext from "zustand/context";
-const { Provider: PusherZustandStoreProvider, useStore: usePusherStore } =
+const { Provider: PusherZustandStoreProvider, useStore: usePusherZustandStore } =
   createContext<StoreApi<PusherZustandStore>>();
 
-import React from "react";
+import React, { useEffect, memo, useMemo } from 'react';
 
+const MemoizedPusherZustandStoreProvider = memo(PusherZustandStoreProvider);
 /**
  * This provider is the thing you mount in the app to "give access to Pusher"
  *
@@ -80,21 +92,12 @@ import React from "react";
 export const PusherProvider: React.FC<
   React.PropsWithChildren<{ slug: string }>
 > = ({ slug, children }) => {
-  const store = React.useMemo<StoreApi<PusherZustandStore>>(
-    () => createPusherStore(slug),
-    [slug]
-  );
-
-  React.useEffect(() => {
-    return () => {
-      store.getState().pusherClient.disconnect();
-    };
-  }, [store]);
+  const store = usePusherStore(slug);
 
   return (
-    <PusherZustandStoreProvider createStore={() => reactZustandCreate(store)}>
+    <MemoizedPusherZustandStoreProvider createStore={() => store}>
       {children}
-    </PusherZustandStoreProvider>
+    </MemoizedPusherZustandStoreProvider>
   );
 };
 
@@ -109,7 +112,7 @@ export function useSubscribeToEvent<MessageType>(
   eventName: string,
   callback: (data: MessageType) => void
 ) {
-  const channel = usePusherStore((state) => state.channel);
+  const channel = usePusherZustandStore((state) => state.channel);
 
   const stableCallback = React.useRef(callback);
 
@@ -130,4 +133,4 @@ export function useSubscribeToEvent<MessageType>(
 }
 
 export const useCurrentMemberCount = () =>
-  usePusherStore((s) => Object.keys(s.members).length);
+  usePusherZustandStore((s) => Object.keys(s.members).length);
