9ccca27989384b063ee052403713c7946ea8db06
MirkoPassoli <mirko.passoli@gmail.com>
2022-07-20 15:04:54 +0200

    Impl. login ui in ask page

--- src/pages/ask/[username].tsx
+++ src/pages/ask/[username].tsx
@@ -8,11 +8,72 @@ import { useState } from "react";
 import { trpc } from "../../utils/trpc";
 import { prisma } from "../../server/db/client";
 import type { User } from "@prisma/client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { FaTwitch } from "react-icons/fa";
 
-const AskForm = (props: { user: User }) => {
-  if (!props.user) throw new Error("user exists Next, sorry");
+const InputForm: React.FC<{
+  userId: string;
+  requiresLogin: boolean;
+}> = ({ userId, requiresLogin }) => {
   const { mutate } = trpc.proxy.questions.submit.useMutation();
   const [question, setQuestion] = useState("");
+
+  const { data: session } = useSession();
+
+  if (requiresLogin && !session) {
+    return (
+      <div className="flex grow flex-col items-center justify-center">
+        <div className="text-lg font-medium">You must login to ask a question</div>
+        <div className="p-1" />
+        <button
+          onClick={() => signIn("twitch")}
+          className="flex items-center gap-2 rounded bg-gray-200 py-2 px-8 font-bold text-gray-800 hover:bg-gray-100"
+        >
+          <span>Sign in with Twitch</span>
+          <FaTwitch />
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <input
+        placeholder="Type something..."
+        className="w-full rounded px-2 py-1 text-center text-lg text-gray-800"
+        type="text"
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+      />
+      <div className="p-4 flex gap-2">
+        <button
+          className="flex items-center rounded bg-gray-200 py-2 px-8 font-bold text-gray-800 hover:bg-gray-100"
+          onClick={() => {
+            if (!question) return;
+
+            mutate({ userId, question });
+
+            setQuestion("");
+          }}
+        >
+          Submit
+        </button>
+
+        {
+          requiresLogin && <button
+            onClick={() => signOut()}
+            className="flex items-center rounded border-2 border-gray-200 py-2 px-8 font-bold text-gray-200 hover:bg-gray-100 hover:text-gray-800"
+          >
+            Logout
+          </button>
+        }
+      </div>
+    </>
+  )
+}
+
+const AskForm = (props: { user: User, requiresLogin: boolean }) => {
+  if (!props.user) throw new Error("user exists Next, sorry");
 
   return (
     <>
@@ -34,26 +95,7 @@ const AskForm = (props: { user: User }) => {
             Ask {props.user?.name} a question!
           </h1>
           <div className="p-4" />
-          <input
-            placeholder="Type something..."
-            className="w-full rounded px-2 py-1 text-center text-lg text-gray-800"
-            type="text"
-            value={question}
-            onChange={(e) => setQuestion(e.target.value)}
-          />
-          <div className="p-4" />
-          <button
-            className="flex rounded bg-gray-200 py-2 px-8 font-bold text-gray-800 hover:bg-gray-100"
-            onClick={() => {
-              if (!question) return;
-
-              mutate({ userId: props.user.id, question });
-
-              setQuestion("");
-            }}
-          >
-            Submit
-          </button>
+          <InputForm userId={props.user.id} requiresLogin={props.requiresLogin} />
         </div>
       </div>
     </>
@@ -78,7 +120,15 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     };
   }
 
-  return { props: { user: userInfo }, revalidate: 60 };
+  const setting = await prisma.settings.findFirst({
+    where: { userId: userInfo.id },
+    select: { requiresLogin: true },
+  })
+
+  // If setting is not found, fallback to login not required
+  const requiresLogin = setting?.requiresLogin ?? false;
+
+  return { props: { user: userInfo, requiresLogin }, revalidate: 60 };
 };
 
 export async function getStaticPaths() {
