3590be07f20e03e2abb6ec2cd7ec7c01e8239feb
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2022-08-11 23:50:39 +0100

    lints with prettier

--- src/pages/ask/[username].tsx
+++ src/pages/ask/[username].tsx
@@ -14,37 +14,54 @@ const AskForm = (props: { user: User }) => {
   const { mutate } = trpc.proxy.questions.submit.useMutation();
   const [question, setQuestion] = useState("");
 
-	return (
-		<>
-			<Head>
-				<title>{`Ask ${props.user?.name} a question!`}</title>
-			</Head>
-			<div className="flex flex-col items-center text-center">
-				<div className="p-14" />
-				<form
-					className="flex w-full max-w-lg flex-col items-center rounded border border-gray-500 bg-gray-600 p-8"
-					onSubmit={(e) => {
-						e.preventDefault();
-						if (!question) return;
+  return (
+    <>
+      <Head>
+        <title>{`Ask ${props.user?.name} a question!`}</title>
+      </Head>
+      <div className="flex flex-col items-center text-center">
+        <div className="p-14" />
+        <form
+          className="flex w-full max-w-lg flex-col items-center rounded border border-gray-500 bg-gray-600 p-8"
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (!question) return;
 
-						mutate({ userId: props.user.id, question });
+            mutate({ userId: props.user.id, question });
 
-						setQuestion("");
-					}}
-				>
-					{props.user.image && <img src={props.user.image} className="fixed top-14 h-28 w-28 rounded-full border-4 border-gray-500" alt="Pro pic" />}
-					<div className="p-4" />
-					<h1 className="text-2xl font-bold">Ask {props.user?.name} a question!</h1>
-					<div className="p-4" />
-					<input placeholder="Type something..." className="w-full rounded px-2 py-1 text-center text-lg text-gray-800" type="text" value={question} onChange={(e) => setQuestion(e.target.value)} />
-					<div className="p-4" />
-					<button className="flex rounded bg-gray-200 py-2 px-8 font-bold text-gray-800 hover:bg-gray-100" type="submit">
-						Submit
-					</button>
-				</form>
-			</div>
-		</>
-	);
+            setQuestion("");
+          }}
+        >
+          {props.user.image && (
+            <img
+              src={props.user.image}
+              className="fixed top-14 h-28 w-28 rounded-full border-4 border-gray-500"
+              alt="Pro pic"
+            />
+          )}
+          <div className="p-4" />
+          <h1 className="text-2xl font-bold">
+            Ask {props.user?.name} a question!
+          </h1>
+          <div className="p-4" />
+          <input
+            placeholder="Type something..."
+            className="w-full rounded px-2 py-1 text-center text-lg text-gray-800"
+            type="text"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+          />
+          <div className="p-4" />
+          <button
+            className="flex rounded bg-gray-200 py-2 px-8 font-bold text-gray-800 hover:bg-gray-100"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
