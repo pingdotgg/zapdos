4c7c96d25838baba2dac4584030fc864f1820679
Mark R. Florkowski <mark.florkowski@gmail.com>
2022-11-14 13:53:42 -0800

    add endpoint for submitting questions via fossabot

---
+++ src/pages/api/external/fossabot.tsx
@@ -0,0 +1,62 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../server/db/client";
+
+const handleRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  const validateUrl = req.headers["x-fossabot-validateurl"] as string;
+  const channelName = req.headers["x-fossabot-channeldisplayname"] as string;
+
+  if (!validateUrl || !channelName) {
+    res.status(400).json({
+      message: "Invalid request",
+    });
+    return;
+  }
+
+  //find user in database
+  const user = await prisma.user.findFirst({
+    where: { name: { equals: channelName } },
+  });
+
+  if (!user) {
+    res.status(400).json({ message: "User not found" });
+    return;
+  }
+
+  //validate request is coming from fossabot
+  const validateResponse = await fetch(validateUrl);
+
+  if (validateResponse.status !== 200) {
+    res.status(400).json({
+      message: "Failed to validate request.",
+    });
+    return;
+  }
+
+  const messageDataUrl = await validateResponse
+    .json()
+    .then((data) => data.context_url);
+
+  const messageDataResponse = await fetch(messageDataUrl);
+
+  if (messageDataResponse.status !== 200) {
+    res.status(400).json({ message: "Failed to fetch message data" });
+    return;
+  }
+
+  const messageData = await messageDataResponse.json();
+
+  // strip off the command, e.g. !ask
+  const question = messageData.message.content.match(/(?<=\s).*/)[0];
+
+  // insert question into database
+  prisma.question.create({
+    data: {
+      body: question,
+      userId: user.id,
+    },
+  });
+
+  res.status(200);
+};
+
+export default handleRequest;
