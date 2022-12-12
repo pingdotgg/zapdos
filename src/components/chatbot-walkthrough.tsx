39518eaffab0b636a3723167781d2ba7df8db792
Brynn Hawker <brynn@hawker.me>
2022-12-12 15:18:55 -0800

    Tabz

--- src/components/chatbot-walkthrough.tsx
+++ src/components/chatbot-walkthrough.tsx
@@ -1,8 +1,53 @@
+import { Tab } from "@headlessui/react";
+import clsx from "clsx";
 import React from "react";
+import Button from "./button";
+
+const TABS = [
+  {
+    label: "Fossabot",
+    content: (
+      <>
+        <p>Fossabot is our recommended bot (says Mark).</p>
+        <pre>{`$(customapi https://ask.ping.gg/api/external/fossabot)`}</pre>
+        <p>
+          Messages sent to this command on your channel will be added to your
+          question queue.
+        </p>
+      </>
+    ),
+  },
+  {
+    label: "Nightbot",
+    content: (
+      <>
+        <p>Fossabot is our recommended bot (says Mark).</p>
+        <pre>{`$(customapi https://ask.ping.gg/api/external/fossabot)`}</pre>
+        <p>
+          Messages sent to this command on your channel will be added to your
+          question queue.
+        </p>
+      </>
+    ),
+  },
+  {
+    label: "StreamElements",
+    content: (
+      <>
+        <p>Fossabot is our recommended bot (says Mark).</p>
+        <pre>{`$(customapi https://ask.ping.gg/api/external/fossabot)`}</pre>
+        <p>
+          Messages sent to this command on your channel will be added to your
+          question queue.
+        </p>
+      </>
+    ),
+  },
+];
 
 export const ChatbotWalkthrough: React.FC = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <div className="prose prose-sm px-4 py-2">
         <p>
           Connecting a chatbot allows your viewers to ask questions by typing a
@@ -12,25 +57,35 @@ export const ChatbotWalkthrough: React.FC = () => {
         <p>
           Ping Ask currently supports Fossabot, Nightbot, and StreamElements.
         </p>
-        <h3>Fossabot</h3>
-        <p>
-          <pre>{`$(customapi https://ask.ping.gg/api/external/fossabot)`}</pre>
-        </p>
-        <h3>Nightbot</h3>
-        <p>
-          <pre>
-            {`$(urlfetch https://ask.ping.gg/api/external/chatbots?q=$(querystring)&channel=$(channel)&user=$(user))`}
-          </pre>
-        </p>
-        <h3>StreamElementst</h3>
-        <p>
-          <pre>{`$\{urlfetch https://ask.ping.gg/api/external/chatbots?channel=$\{channel}&q=$\{queryescape $\{1:}}&user=$\{user}}`}</pre>
-        </p>
-        <p>
-          Messages sent to this command on your channel will be added to your
-          question queue.
-        </p>
       </div>
+      <Tab.Group>
+        <Tab.List className="border-b border-gray-700 px-2">
+          {TABS.map(({ label }) => (
+            <Tab as={React.Fragment} key={label}>
+              {({ selected }) => (
+                /* Use the `selected` state to conditionally style the selected tab. */
+                <Button
+                  variant={selected ? "secondary" : "ghost"}
+                  className={clsx(
+                    "-mb-px rounded-b-none border-b-0 ",
+                    selected ? "bg-gray-850" : "bg-transparent"
+                  )}
+                  key={label}
+                >
+                  {label}
+                </Button>
+              )}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels>
+          {TABS.map(({ label, content }) => (
+            <Tab.Panel key={label} className="prose prose-sm p-4">
+              {content}
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 };
